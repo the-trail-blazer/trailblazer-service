38db1a8623521f02bf150471ec0ecf05fe03da57
finn <isaaclindland@protonmail.com>
2019-10-15 13:57:31 -0600

    initial commit

---
+++ src/main/java/io/trailblazer/trailblazerservice/TrailblazerServiceApplication.java
@@ -0,0 +1,13 @@
+package io.trailblazer.trailblazerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailblazerServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrailblazerServiceApplication.class, args);
+  }
+
+}
