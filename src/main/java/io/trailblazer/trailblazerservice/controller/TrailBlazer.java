ca234c00ac36ebcd2a0b5117ce07d600afea5560
finn <isaaclindland@protonmail.com>
2019-10-16 16:44:28 -0600

    added controller

---
+++ src/main/java/io/trailblazer/trailblazerservice/controller/TrailBlazer.java
@@ -0,0 +1,14 @@
+package io.trailblazer.trailblazerservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trails")
+public class TrailBlazer {
+
+
+
+
+}
